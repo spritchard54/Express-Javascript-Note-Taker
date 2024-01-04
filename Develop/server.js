902013f00235c6123b2360df5b927842f89f9293
spritchard54 <>
2024-01-04 18:31:41 -0500

    New notes are can be seen on resfresh

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,17 @@
 //Import Express.js
 const express = require('express');
-
-const fs = require('fs/promises');
-
+const fs = require('fs');
+const util = require('util');
 //Import built in Node.js package 'path' to reslve path of files that are located on the server.
 const path = require('path');
-
-const router = require('express').Router();
-
+// const router = require('express').Router();
 const uuid = require('./helpers/uuid');
-
 //Initialize an instance of Express.js
 const app = express();
-
 // allows server.js to access the database file 'db.json'
 const dataBase = require('./db/db.json');
-
 //Specify on which port the express.js server will run
 const PORT = 3001;
-
 //Middleware pointing to static files in the public folder. Creates routes for static files.
 app.use(express.static('public'));
 
@@ -37,23 +30,25 @@ app.get('/api/notes', (req, res) => {
     // console.log("This is the required path:" + "'" + req.path + "'");
 });
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+
 // POST request
 app.post('/api/notes', async (req, res) => {
     // Let the client know that their POST request was received
     // res.json(`${req.method} request received`);
 
-    const oldNotes = await fs.readFile('./db/db.json', 'utf-8')
+    const oldNotes = await readFile('./db/db.json', 'utf-8')
     // console.log(oldNotes);
-    const parsedNotes = await JSON.parse(oldNotes);
-    // Show the user agent information in the terminal
-console.log('we are here');
+    console.log('we are here');
     // Log our request to the terminal
     console.info(`${req.method} request received`);
 
-    const { title, text } = await req.body;
-console.log('you are there');
-console.log(req.body);
-console.log(uuid);
+    const { title, text } = req.body;
+    console.log('you are there');
+    console.log(req.body);
+    console.log(uuid);
     // If all the required properties are present
     if (title && text) {
         // Variable for the object we will save
@@ -62,16 +57,20 @@ console.log(uuid);
             title: title,
             text: text
         };
-console.log(newNote);
-        parsedNotes.concat(newNote)
-        fs.writeFile('./db/db.json', JSON.stringify(oldNotes));
+        console.log(newNote);
+        console.log(typeof JSON.parse(oldNotes));
+        console.log(oldNotes + "These are the old notes");
+        const parsedNotes = await JSON.parse(oldNotes);
+        parsedNotes.push(newNote)
+        writeFile('./db/db.json', JSON.stringify(parsedNotes));
 
         const response = {
             status: 'success',
             body: newNote,
         };
 
-console.log(response);
+        console.log(response);
+       
         res.status(201).json(response);
     } else {
         res.status(500).json('Error in posting note');
