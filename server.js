36f69bd5710f16bb6eac69afb0bed400f1f8a96d
spritchard54 <>
2024-01-08 18:08:31 -0500

    Removed develop folder

--- server.js
+++ server.js
@@ -2,41 +2,41 @@ const express = require('express')
 const fs = require('fs')
 // const util = require('util')
 const path = require('path')
-const uuid = require('./Develop/helpers/uuid')
+const uuid = require('./helpers/uuid')
 const app = express()
-const dataBase = require('./Develop/db/db.json')
+const dataBase = require('./db/db.json')
 const { title } = require('process')
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(__dirname + '/develop/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // --------------------------- Get starts here ------------------------------------
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public', 'index.html'))
+  res.sendFile(path.join(__dirname, './public', 'index.html'))
   // Log our request to the terminal
   console.info(`${req.method} 1. app.get('/' index.html request received`)
 })
 
 // Retrieves /notes when you click 'Get Started' Button on 'index.html'
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public', 'notes.html'))
+  res.sendFile(path.join(__dirname, './public', 'notes.html'))
   // Log our request to the terminal
   console.info(`${req.method} 2. app.get('/notes.html request received`)
 })
 
 // GET notes from json file and display old notes on /notes.html
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/db', 'db.json'))
+  res.sendFile(path.join(__dirname, './db', 'db.json'))
   console.info(`${req.method} 3. /api/notes received`)
 })
 
 // --------------------------- Post starts here ------------------------------------
 
 app.post('/api/notes', (req, res) => {
-  const oldNotes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf-8'))
+  const oldNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
   const { title, text } = req.body
 
   if (title && text) {
@@ -50,7 +50,7 @@ app.post('/api/notes', (req, res) => {
     console.log(newNote);
 
     oldNotes.push(newNote)
-    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(oldNotes))
+    fs.writeFileSync('./db/db.json', JSON.stringify(oldNotes))
     res.json(oldNotes)
 
     // Log our request to the terminal
